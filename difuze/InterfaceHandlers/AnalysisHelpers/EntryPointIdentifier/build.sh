f6d1e7b9046416f47314b379968567c7107ac9ad
sonix03 <13523121@std.stei.itb.ac.id>
2026-02-08 21:30:12 +0700

    add: difuzer

--- difuze/InterfaceHandlers/AnalysisHelpers/EntryPointIdentifier/build.sh
+++ difuze/InterfaceHandlers/AnalysisHelpers/EntryPointIdentifier/build.sh
@@ -8,5 +8,15 @@ if [[ -z "$LLVM_CONFIG_BIN" ]]; then
   exit 1
 fi
 
+LLVM_CXXFLAGS="$("$LLVM_CONFIG_BIN" --cxxflags)"
+LLVM_LDFLAGS="$("$LLVM_CONFIG_BIN" --ldflags)"
+LLVM_SYS_LIBS="$("$LLVM_CONFIG_BIN" --system-libs)"
+if LLVM_LIBS="$("$LLVM_CONFIG_BIN" --link-shared --libs 2>/dev/null)" && [[ -n "$LLVM_LIBS" ]]; then
+  :
+else
+  LLVM_LIBS="$("$LLVM_CONFIG_BIN" --libs core irreader bitreader bitwriter support)"
+fi
+
+# shellcheck disable=SC2086
 g++ "$BASEDIR/src/main.cpp" -fpermissive -o "$BASEDIR/entry_point_handler" \
-  $("$LLVM_CONFIG_BIN" --cxxflags --ldflags --libs all --system-libs)
+  $LLVM_CXXFLAGS $LLVM_LDFLAGS $LLVM_LIBS $LLVM_SYS_LIBS
